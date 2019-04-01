16c76c1b24c6d868d65a4367e59bc96f988c6427
hortencia718 <48457681+hortencia718@users.noreply.github.com>
2019-04-01 17:26:28 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+
+
+<!doctype html>
+<html lang="en">
+<head>
+   
+    <body>
+     <title>hortencia cisneros photography</title>
+     <section>
+         <img
+     </section>
+    
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="author" content="hortencia cisneros">
+    <meta name="keywords" content="website,photography">
+    <meta name="description" content="web design,website,photography">
+    
+        <link rel="stylesheet" href="css/stylesheet.css">
+      <link rel="stylesheet" href=https://use.fontawesome.com>
+  </head>
+  
+     
+   <div>
+         <img src="welcome.png"/>
+    <div class="main content">
+    <div id= nav class="clear">
+      <div class= "logo">
+         <img src="img/stills/logows3" jpg alt="logows3jpg">
+         
+        <br>
+        <h2>hover tabs</h2>
+         <p>move the mouse over a button inside the tabbed menu</p>
+          <div class="tab"></div>
+          
+        <ul class="navbar" id="myTopnav">
+            
+            <li><a href="home"></a>home</li>
+            <br>
+            <li><a href="travel">travel</a></li>
+            <br>
+            <li><a href="food">food</a></li>
+            <br>
+            <li><a href="stills">stills</a></li>
+            <br>
+            <li><a href="contact">contact</a></li>
+        </ul>
+      </div>
+       <div class="image-container float center">
+          <img src="/web/IMG_4053.jpg">
+          
+         
+              <div id= class image-container clear>
+          <img src="/web/ DSC_0666 copy.jpg" alt= "owl broch">
+          
+          
+          <meta name="copyright">
+          
+          
+          
+          <footer class="footer">
+              </footer>
+         
+    
+   
+   </head>
+  </body>
+
+
+</html>
